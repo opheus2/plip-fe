9e8771729770e9c20e08397452d693c1c9579d7b
opheus2 <j.ominiabohs@gmail.com>
2021-12-13 17:32:29 +0800

    add  echo client

---
+++ utils/EchoClient.js
@@ -0,0 +1,41 @@
+import Echo from 'laravel-echo'
+import Http from '../Utils/Http'
+
+export function EchoClient(authToken) {
+   const auth = {}
+   const options = {
+      broadcaster: 'pusher',
+      key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY,
+      cluster: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTER,
+      forceTLS: false,
+      encrypted: false,
+      authorizer: (channel, options) => {
+         return {
+            authorize: (socketId, callback) => {
+               Http()
+                  .post(
+                     '/broadcasting/auth',
+                     {
+                        socket_id: socketId,
+                        channel_name: channel.name,
+                     },
+                     {
+                        headers: {
+                           Authorization: `Bearer ${authToken}`,
+                           'X-Socket-ID': socketId,
+                        },
+                     }
+                  )
+                  .then((response) => {
+                     callback(false, response.data)
+                  })
+                  .catch((error) => {
+                     callback(true, error)
+                  })
+            },
+         }
+      },
+   }
+
+   return new Echo(options)
+}
